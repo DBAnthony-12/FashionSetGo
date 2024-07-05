032818229cc6f311360c40d7639e0a574aa738fe
DBAnthony-12 <126782224+DBAnthony-12@users.noreply.github.com>
2024-07-05 10:51:51 +0200

    Added more changes to the website. Updated Website

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const sidebar = document.querySelector(".sidebar");
 const closeSidebar = document.getElementById("closeSidebar");
 
 menuIcon.addEventListener("click", function () {
-  sidebar.style.width = "200px"; // Adjust width to match sidebar width in CSS
+  sidebar.classList.add("active"); // Adjust width to match sidebar width in CSS
 });
 
 closeSidebar.addEventListener("click", function () {
-  sidebar.style.width = "0px"; // Close sidebar by setting width to 0px
+  sidebar.classList.remove("active"); // Close sidebar by setting width to 0px
 });
 
 // Carousel
